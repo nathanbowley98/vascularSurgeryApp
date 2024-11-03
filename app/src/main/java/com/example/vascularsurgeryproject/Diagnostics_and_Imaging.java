e9405d4cc05f320424a657ed6a8c49459efad1bb
Julia <julia.zaki@mail.utoronto.ca>
2024-11-03 14:50:38 -0500

    Task 5 Julia

--- app/src/main/java/com/example/vascularsurgeryproject/Diagnostics_and_Imaging.java
+++ app/src/main/java/com/example/vascularsurgeryproject/Diagnostics_and_Imaging.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Anatomy extends AppCompatActivity {
+public class Diagnostics_and_Imaging extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_anatomy2);
+        setContentView(R.layout.activity_diagnostics_and_imaging);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);

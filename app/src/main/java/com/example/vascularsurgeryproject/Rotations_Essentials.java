964ad2a7fb61236470271becde48a4e0d18e4f5b
Oxbower <83982029+Oxbower@users.noreply.github.com>
2025-01-05 00:46:32 -0400

    Jan 04 - 05, 2025 - Added BaseActivity functionality to Rotations_Essentials page - Fixed the XML for the Rotations_Essentials page

--- app/src/main/java/com/example/vascularsurgeryproject/Rotations_Essentials.java
+++ app/src/main/java/com/example/vascularsurgeryproject/Rotations_Essentials.java
@@ -8,17 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Rotations_Essentials extends AppCompatActivity {
+public class Rotations_Essentials extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+//        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_rotations_essentials);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
     }
 }

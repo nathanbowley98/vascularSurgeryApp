511fe54adeae4e8cc464501ad175b9c866f85ad9
Oxbower <83982029+Oxbower@users.noreply.github.com>
2025-01-08 19:01:23 -0400

    made the toolbar and bottom bar work

--- app/src/main/java/com/example/vascularsurgeryproject/Rotations_Essentials.java
+++ app/src/main/java/com/example/vascularsurgeryproject/Rotations_Essentials.java
@@ -13,12 +13,9 @@ public class Rotations_Essentials extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_rotations_essentials);
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
+
+        setupToolbar();
+        setupBottomBar();
     }
 }

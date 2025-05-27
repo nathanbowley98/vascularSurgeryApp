c9821c09160b634061ff967a0e7e18e3d9bd6e15
Dimitrios <dimitrios.gkiokmema@mail.utoronto.ca>
2025-05-27 15:48:39 -0400

    Aortic Dissection page created and loaded with actual data.

--- app/src/main/java/com/example/vascularsurgeryproject/Aortic_Dissection.java
+++ app/src/main/java/com/example/vascularsurgeryproject/Aortic_Dissection.java
@@ -2,10 +2,18 @@ package com.example.vascularsurgeryproject;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class Aortic_Dissection extends BaseActivity {
     @Override
@@ -13,19 +21,54 @@ public class Aortic_Dissection extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aortic_dissection);
 
-//        LinearLayout buttonContainer = findViewById(R.id.mainPageButtons);
-//        LayoutInflater inflater = LayoutInflater.from(this);
-//
-//        for (int i = 0; i < 5; i++) {
-//            // Inflate the subpage button layout
-//            View buttonLayout = inflater.inflate(R.layout.activity_subpage_button, buttonContainer, false);
-//
-//            Button button = buttonLayout.findViewById(R.id.pageBtn);
-//            bindButtonListener(button, new Intent(com.example.vascularsurgeryproject.Pathophysiology.this, .class));
-//            button.setText("Button " + (i + 1));
-//
-//            // Add to parent layout
-//            buttonContainer.addView(buttonLayout);
-//        }
+        try {
+            populatePage();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void populatePage() throws IOException {
+        // Reads the data in the txt file and displays them on the page
+        InputStream inputStream = getResources().openRawResource(R.raw.aortic_dissection);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        String contentTitle = "";
+        StringBuilder content = new StringBuilder();
+        String line;
+
+        LinearLayout textContainer = findViewById(R.id.scrollview_container);
+        LayoutInflater inflater = LayoutInflater.from(this);
+
+        // Iterates over data in csv file
+        while ((line = reader.readLine()) != null) {
+            if (line.equals("<end>")) {
+                // End of text section, create paragraph on page and reset variables
+                View textBlockLayout = inflater.inflate(R.layout.activity_text_block, textContainer, false);
+
+                // Add title
+                TextView titleView = textBlockLayout.findViewById(R.id.title);
+                titleView.setText(contentTitle);
+
+                // Add content
+                TextView contentView = textBlockLayout.findViewById(R.id.content);
+                contentView.setText(content.toString());
+
+                // Add to parent layout
+                textContainer.addView(textBlockLayout);
+
+                // Reset saved text
+                contentTitle = "";
+                content = new StringBuilder();
+            } else if (content.toString().equals("<start>")) {
+                // If this condition is met, then the last line was <start>, so now we are at
+                // the title of a new text section
+                contentTitle = line;
+                content = new StringBuilder();
+            } else {
+                // In the middle of a text section, simply add the current line to our saved
+                // content
+                content.append(line);
+            }
+        }
     }
 }
